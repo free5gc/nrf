c539673168a2bd17a615a3807345265ef8cc22d3
ianchen0119 <ychen.desl@gmail.com>
2023-10-26 09:04:17 +0000

    remove unused attr

--- internal/sbi/management/api_nf_instance_id_document.go
+++ internal/sbi/management/api_nf_instance_id_document.go
@@ -27,6 +27,7 @@ func HTTPDeregisterNFInstance(c *gin.Context) {
 	oauth_err := openapi.VerifyOAuth(c.Request.Header.Get("Authorization"), "nnrf-nfm",
 		factory.NrfConfig.GetNrfCertPemPath())
 	if oauth_err != nil && factory.NrfConfig.GetOAuth() {
+		logger.NfmLog.Warnln(oauth_err.Error())
 		c.JSON(http.StatusUnauthorized, gin.H{"error": oauth_err.Error()})
 		return
 	}
