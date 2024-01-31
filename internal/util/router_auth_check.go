7e6b144db9bd8b419d2b1b39362c80586586f0f0
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2024-01-31 04:02:23 +0000

    Fix: fix linter error

--- internal/util/router_auth_check.go
+++ internal/util/router_auth_check.go
@@ -22,7 +22,6 @@ func NewRouterAuthorizationCheck(serviceName string) *RouterAuthorizationCheck {
 func (rac *RouterAuthorizationCheck) Check(c *gin.Context, pcfContext nrf_context.NFContext) {
 	token := c.Request.Header.Get("Authorization")
 	err := pcfContext.AuthorizationCheck(token, rac.serviceName)
-
 	if err != nil {
 		logger.UtilLog.Debugf("RouterAuthorizationCheck::Check Unauthorized: %s", err)
 		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
