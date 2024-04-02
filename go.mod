8b09ae21bdee29932029f27433294d2f9a0be0e3
ianchen0119 <ychen.desl@gmail.com>
2024-04-02 06:25:35 +0000

    3. Implement Server structure & refactor api_{SVC}.go

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/free5gc/openapi v1.0.8
+	github.com/free5gc/openapi-r17 v0.0.0
 	github.com/free5gc/util v1.0.6
 	github.com/gin-gonic/gin v1.9.1
 	github.com/golang-jwt/jwt v3.2.1+incompatible
@@ -21,15 +22,17 @@ replace github.com/free5gc/openapi-r17 v0.0.0 => ./openapi
 
 require (
 	github.com/antihax/optional v1.0.0 // indirect
-	github.com/bytedance/sonic v1.9.1 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/bytedance/sonic v1.11.3 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/evanphx/json-patch v0.5.2 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/cors v1.7.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.14.0 // indirect
+	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
@@ -37,28 +40,28 @@ require (
 	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
-	github.com/leodido/go-urn v1.2.4 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/tim-ywliu/nested-logrus-formatter v1.3.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/arch v0.7.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
---
+++ internal/sbi/server.go
@@ -0,0 +1,245 @@
+package sbi
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime/debug"
+	"sync"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+
+	nrf_context "github.com/free5gc/nrf/internal/context"
+	"github.com/free5gc/nrf/internal/logger"
+	"github.com/free5gc/nrf/internal/sbi/processor"
+	"github.com/free5gc/nrf/pkg/factory"
+	"github.com/free5gc/openapi-r17"
+	"github.com/free5gc/openapi-r17/models"
+	"github.com/free5gc/util/httpwrapper"
+)
+
+const (
+	CorsConfigMaxAge = 86400
+)
+
+type Endpoint struct {
+	Method  string
+	Pattern string
+	APIFunc gin.HandlerFunc
+}
+
+func applyEndpoints(group *gin.RouterGroup, endpoints []Endpoint) {
+	for _, endpoint := range endpoints {
+		switch endpoint.Method {
+		case "GET":
+			group.GET(endpoint.Pattern, endpoint.APIFunc)
+		case "POST":
+			group.POST(endpoint.Pattern, endpoint.APIFunc)
+		case "PUT":
+			group.PUT(endpoint.Pattern, endpoint.APIFunc)
+		case "PATCH":
+			group.PATCH(endpoint.Pattern, endpoint.APIFunc)
+		case "DELETE":
+			group.DELETE(endpoint.Pattern, endpoint.APIFunc)
+		}
+	}
+}
+
+type nrf interface {
+	Config() *factory.Config
+	Context() *nrf_context.NRFContext
+	CancelContext() context.Context
+	Processor() *processor.Processor
+}
+
+type Server struct {
+	nrf
+
+	httpServer *http.Server
+	router     *gin.Engine
+}
+
+func NewServer(nrf nrf, tlsKeyLogPath string) (*Server, error) {
+	s := &Server{
+		nrf: nrf,
+	}
+
+	endpoints := s.getAccessTokenEndpoints()
+	group := s.router.Group(openapi.ServiceBaseUri(models.ServiceName_NNRF_OAUTH2))
+	applyEndpoints(group, endpoints)
+
+	s.router.Use(cors.New(cors.Config{
+		AllowMethods: []string{"GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"},
+		AllowHeaders: []string{
+			"Origin", "Content-Length", "Content-Type", "User-Agent",
+			"Referrer", "Host", "Token", "X-Requested-With",
+		},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowAllOrigins:  true,
+		MaxAge:           CorsConfigMaxAge,
+	}))
+
+	cfg := s.Config()
+	bindAddr := cfg.GetSbiBindingAddr()
+	logger.SBILog.Infof("Binding addr: [%s]", bindAddr)
+	var err error
+	if s.httpServer, err = httpwrapper.NewHttp2Server(bindAddr, tlsKeyLogPath, s.router); err != nil {
+		logger.InitLog.Errorf("Initialize HTTP server failed: %v", err)
+		return nil, err
+	}
+	s.httpServer.ErrorLog = log.New(logger.SBILog.WriterLevel(logrus.ErrorLevel), "HTTP2: ", 0)
+
+	return s, nil
+}
+
+func (s *Server) Run(traceCtx context.Context, wg *sync.WaitGroup) error {
+	wg.Add(1)
+	go s.startServer(wg)
+	return nil
+}
+
+func (s *Server) Stop(traceCtx context.Context) {
+	const defaultShutdownTimeout time.Duration = 2 * time.Second
+
+	if s.httpServer != nil {
+		logger.SBILog.Infof("Stop SBI server (listen on %s)", s.httpServer.Addr)
+		toCtx, cancel := context.WithTimeout(context.Background(), defaultShutdownTimeout)
+		defer cancel()
+		if err := s.httpServer.Shutdown(toCtx); err != nil {
+			logger.SBILog.Errorf("Could not close SBI server: %#v", err)
+		}
+	}
+}
+
+func (s *Server) startServer(wg *sync.WaitGroup) {
+	defer func() {
+		if p := recover(); p != nil {
+			// Print stack for panic to log. Fatalf() will let program exit.
+			logger.SBILog.Fatalf("panic: %v\n%s", p, string(debug.Stack()))
+		}
+
+		wg.Done()
+	}()
+
+	logger.SBILog.Infof("Start SBI server (listen on %s)", s.httpServer.Addr)
+
+	var err error
+	cfg := s.Config()
+	scheme := cfg.GetSbiScheme()
+	if scheme == "http" {
+		err = s.httpServer.ListenAndServe()
+	} else if scheme == "https" {
+		err = s.httpServer.ListenAndServeTLS(
+			cfg.GetCertPemPath(),
+			cfg.GetCertKeyPath())
+	} else {
+		err = fmt.Errorf("No support this scheme[%s]", scheme)
+	}
+
+	if err != nil && err != http.ErrServerClosed {
+		logger.SBILog.Errorf("SBI server error: %v", err)
+	}
+	logger.SBILog.Warnf("SBI server (listen on %s) stopped", s.httpServer.Addr)
+}
+
+func checkContentTypeIsJSON(gc *gin.Context) (string, error) {
+	var err error
+	contentType := gc.GetHeader("Content-Type")
+	if openapi.KindOfMediaType(contentType) != openapi.MediaKindJSON {
+		err = fmt.Errorf("Wrong content type %q", contentType)
+	}
+
+	if err != nil {
+		logger.SBILog.Error(err)
+		gc.JSON(http.StatusInternalServerError,
+			openapi.ProblemDetailsMalformedReqSyntax(err.Error()))
+		return "", err
+	}
+
+	return contentType, nil
+}
+
+func (s *Server) deserializeData(gc *gin.Context, data interface{}, contentType string) error {
+	reqBody, err := gc.GetRawData()
+	if err != nil {
+		logger.SBILog.Errorf("Get Request Body error: %v", err)
+		gc.JSON(http.StatusInternalServerError,
+			openapi.ProblemDetailsSystemFailure(err.Error()))
+		return err
+	}
+
+	err = openapi.Deserialize(data, reqBody, contentType)
+	if err != nil {
+		logger.SBILog.Errorf("Deserialize Request Body error: %v", err)
+		gc.JSON(http.StatusBadRequest,
+			openapi.ProblemDetailsMalformedReqSyntax(err.Error()))
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) bindData(gc *gin.Context, data interface{}) error {
+	err := gc.Bind(data)
+	if err != nil {
+		logger.SBILog.Errorf("Bind Request Body error: %v", err)
+		gc.JSON(http.StatusBadRequest,
+			openapi.ProblemDetailsMalformedReqSyntax(err.Error()))
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) buildAndSendHttpResponse(
+	gc *gin.Context,
+	hdlRsp *processor.HandlerResponse,
+	multipart bool,
+) {
+	if hdlRsp.Status == 0 {
+		// No Response to send
+		return
+	}
+
+	rsp := httpwrapper.NewResponse(hdlRsp.Status, hdlRsp.Headers, hdlRsp.Body)
+
+	buildHttpResponseHeader(gc, rsp)
+
+	var rspBody []byte
+	var contentType string
+	var err error
+	if multipart {
+		rspBody, contentType, err = openapi.MultipartSerialize(rsp.Body)
+	} else {
+		// TODO: support other JSON content-type
+		rspBody, err = openapi.Serialize(rsp.Body, "application/json")
+		contentType = "application/json"
+	}
+
+	if err != nil {
+		logger.SBILog.Errorln(err)
+		gc.JSON(http.StatusInternalServerError, openapi.ProblemDetailsSystemFailure(err.Error()))
+	} else {
+		gc.Data(rsp.Status, contentType, rspBody)
+	}
+}
+
+func buildHttpResponseHeader(gc *gin.Context, rsp *httpwrapper.Response) {
+	for k, v := range rsp.Header {
+		// Concatenate all values of the Header with ','
+		allValues := ""
+		for i, vv := range v {
+			if i == 0 {
+				allValues += vv
+			} else {
+				allValues += "," + vv
+			}
+		}
+		gc.Header(k, allValues)
+	}
+}
